3bc7177cb7f2e7b6a2b67b2727a6b5dd7e32ae88
yuki-fujisaki <yuki.f.bizz@gmail.com>
2023-03-19 12:48:47 +0900

    add: リンク追加

--- nextjs_practice/pages/products/index.tsx
+++ nextjs_practice/pages/products/index.tsx
@@ -1,5 +1,25 @@
+import Link from "next/link";
+import styles from "../../styles/Home.module.css";
+
 const ProductsList = () => {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h2 className={styles.title}>商品一覧</h2>
+        <ul>
+          <li>
+            <Link href="/products/smartphone">スマートフォン</Link>
+          </li>
+          <li>
+            <Link href="/products/pc">パソコン</Link>
+          </li>
+          <li>
+            <Link href="/products/headphone">ヘッドフォン</Link>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 };
 
 export default ProductsList;
