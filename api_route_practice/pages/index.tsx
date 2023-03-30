0c3c1ec0f7a1f16fbd3e240d9c78d0e64d14b3a8
yuki-fujisaki <yuki.f.bizz@gmail.com>
2023-03-30 18:49:44 +0900

    add: UserCard

--- api_route_practice/pages/index.tsx
+++ api_route_practice/pages/index.tsx
@@ -1,11 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
+import { User } from "@prisma/client";
+import UserCard from "../components/UserCard";
 
 export default function Home() {
+  const [users, setUsers] = useState<Pick<User, "id" | "email" | "name">[]>([]);
+
   useEffect(() => {
     (async () => {
       const res = await fetch("/api/users");
       const users = await res.json();
+      setUsers(users);
       console.log(users);
     })();
   }, []);
@@ -13,9 +18,10 @@ export default function Home() {
   return (
     <Layout>
       <div className="flex flex-col justify-center items-center">
-        <h2 className="text-4xl text-black font-sans p-4 mt-6">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h2>
+        <h2 className="text-4xl font-black font-sans p-4 mt-6">User Lists</h2>
+        <div className="flex flex-wrap w-full justify-center items-center gap-3">
+          {users && users.map((user) => <UserCard key={user.id} user={user} />)}
+        </div>
       </div>
     </Layout>
   );
