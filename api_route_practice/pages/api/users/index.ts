585c262014a4b03e4c217e1de4afbe4e6f943632
yuki-fujisaki <yuki.f.bizz@gmail.com>
2023-03-30 16:22:49 +0900

    add: Request Method

--- api_route_practice/pages/api/users/index.ts
+++ api_route_practice/pages/api/users/index.ts
@@ -1,17 +1,42 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../../../utils/prismaClient";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../utils/prismaClient'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const users = await prisma.user.findMany({
-    select: {
-      id: true,
-      name: true,
-      email: true,
-    },
-  });
+  const { method, body } = req
 
-  res.status(200).json(users);
+  switch (method) {
+    case 'GET':
+      const users = await prisma.user.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      })
+      res.status(200).json(users)
+      break
+
+    case 'POST':
+      const createdUser = await prisma.user.create({
+        data: {
+          email: body.email,
+          name: body.name,
+          password: body.password,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      })
+      res.status(200).json(createdUser)
+      break
+
+    default:
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`${method} method not allowed`)
+  }
 }
